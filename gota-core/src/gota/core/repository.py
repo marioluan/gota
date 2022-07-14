155d7ff2c5f7cf24ebd53893c9746ea2380664fe
🍄mariodes🍄 <mariodes@amazon.com>
2022-07-15 00:56:19 +0100

    core: add repository layer

---
+++ gota-core/src/gota/core/repository.py
@@ -0,0 +1,81 @@
+import uuid
+from typing import List, Union
+
+import arrow
+from gota.core.errors import RecipeNotFoundError
+from gota.core.models import Recipe, RecipeID
+from gota.core.storage.istorage import IStorage
+from gota.core.time_converter import datetime_to_milliseconds
+
+
+class RecipeRepository:
+    def __init__(self, storage: IStorage):
+        self._storage = storage
+
+    @staticmethod
+    def _update_duration_total_time_millis(recipe: Recipe) -> None:
+        recipe.duration.total_time_millis = (
+            recipe.duration.cook_time_millis + recipe.duration.preparation_time_millis
+        )
+
+    def get_recipes(self) -> List[Recipe]:
+        """
+        Get all recipes from the repository.
+
+        :return: all recipes from repository
+        """
+        return self._storage.get_items()
+
+    def get_recipe(self, recipe_id: RecipeID) -> Union[Recipe, RecipeNotFoundError]:
+        """
+        Get the recipe with recipe_id from the repository.
+
+        :param recipe_id: the unique identifier of the recipe
+        :raises RecipeNotFoundError: if recipe wasn't found
+        :return: the recipe found
+        """
+        recipe = self._storage.get_item(recipe_id)
+        if not recipe:
+            raise RecipeNotFoundError()
+        return recipe
+
+    def save_recipe(self, recipe: Recipe) -> Recipe:
+        """
+        Save the recipe in the repository.
+
+        :param recipe: the recipe
+        :return: the saved recipe (with auto-generated fields)
+        """
+        # auto-generated
+        recipe.recipe_id = uuid.uuid4()
+        recipe.created_at_millis = datetime_to_milliseconds(arrow.utcnow())
+        self._update_duration_total_time_millis(recipe)
+
+        self._storage.save_item(item_id=recipe.recipe_id, item=recipe)
+
+        return recipe
+
+    def update_recipe(
+        self, recipe_id: RecipeID, recipe: Recipe
+    ) -> Union[None, RecipeNotFoundError]:
+        """
+        Update the recipe with the given recipe_id in the repository.
+
+        :param recipe_id: the unique identifier of the recipe to be updated
+        :param recipe: the recipe to be saved in the repository
+        :raises RecipeNotFoundError: if the recipe isn't present in the repository
+        :return: void
+        """
+        existing_recipe = self.get_recipe(recipe_id=recipe_id)
+
+        # these fields are optional, but once set, we enforce immutability here
+        recipe.recipe_id = existing_recipe.recipe_id
+        recipe.created_at_millis = existing_recipe.created_at_millis
+
+        # auto-generated
+        recipe.updated_at_millis = datetime_to_milliseconds(arrow.utcnow())
+        self._update_duration_total_time_millis(recipe)
+
+        self._storage.save_item(item_id=recipe_id, item=recipe)
+
+        return recipe
---
+++ gota-core/tests/gota/core/test_repository.py
@@ -0,0 +1,48 @@
+import pytest
+
+
+@pytest.mark.skip("It's a private method, but I will test it anyways in a later opportunity.")
+def test__update_duration_total_time_millis():
+    pass
+
+
+@pytest.mark.skip(
+    "This test is expensive to write. I'm going to implement them once the application is built."
+)
+def test_get_recipes():
+    pass
+
+
+@pytest.mark.skip(
+    "This test is expensive to write. I'm going to implement them once the application is built."
+)
+def test_get_recipe_when_found():
+    pass
+
+
+@pytest.mark.skip(
+    "This test is expensive to write. I'm going to implement them once the application is built."
+)
+def test_get_recipe_when_not_found():
+    pass
+
+
+@pytest.mark.skip(
+    "This test is expensive to write. I'm going to implement them once the application is built."
+)
+def test_save_recipe():
+    pass
+
+
+@pytest.mark.skip(
+    "This test is expensive to write. I'm going to implement them once the application is built."
+)
+def test_update_recipe_when_found():
+    pass
+
+
+@pytest.mark.skip(
+    "This test is expensive to write. I'm going to implement them once the application is built."
+)
+def test_update_recipe_when_not_found():
+    pass
